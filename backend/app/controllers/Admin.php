27c4876be67d61c780c07a5c386924bbebad06ce
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-02 19:33:42 +0000

    model de mehodes delete

--- backend/app/controllers/Admin.php
+++ backend/app/controllers/Admin.php
@@ -2,9 +2,15 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Methods:*');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") return true;
 
 class Admin extends Controller
 {
+    public $id;
     public $valide = false;
     public function __construct()
     {
@@ -33,11 +39,11 @@ class Admin extends Controller
 
     public function DELETEUSER()
     {
-        if ($_SERVER["REQUEST_METHOD"] === "delete") {
-            $id = $_GET["id"];
+        if ($_SERVER["REQUEST_METHOD"] === "DELETE") {
+            $this->id = $_GET['id'];
             $admin = $this->model('AdminModel');
-            $admin->remove($id);
-            echo json_encode($id);
+            $admin->remove($this->id);
+            // echo json_encode($id);
         }
     }
     public function DELETERDV()
