a86db5a6468525e5535a625b0be171cc55f1fac5
Halazv2 <halaziani2003@gmail.com>
2022-04-03 15:57:16 +0000

    daisyUI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["dracula"],
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
 };
