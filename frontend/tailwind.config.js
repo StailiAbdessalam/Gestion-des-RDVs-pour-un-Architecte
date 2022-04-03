9687bea3f52da60fc96d1a29be33b09b25dbd3f4
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-03 22:01:39 +0000

    tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,3 +9,4 @@ module.exports = {
     themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
 };
+  
